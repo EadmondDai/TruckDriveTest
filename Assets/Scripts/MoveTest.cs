57a87f38d0fe4e54e38ac0159567f972a013381d
Eadmond <eadmond@aliyun.com>
2016-12-14 14:12:12 -0800

    Moved Project into the team folder.

---
+++ Assets/Scripts/MoveTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTest : MonoBehaviour {
+
+    public float MoveSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float deltaTime = Time.deltaTime;
+
+        float verticalMove = Input.GetAxis("Vertical");
+        transform.Translate(transform.forward * verticalMove * MoveSpeed * deltaTime);
+	}
+}
