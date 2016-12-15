045aa8273bf5c80d5f88e6a36b4707d3661c3f32
Eadmond <eadmond@aliyun.com>
2016-12-14 20:39:04 -0800

    Fixed moving truck rotate problem.

--- Assets/Scripts/MoveTest.cs
+++ Assets/Scripts/MoveTest.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//© Eadmond, 12.14.2016
+// This is for testing the moving ability of our truck.
+
 public class MoveTest : MonoBehaviour {
 
     public float MoveSpeed = 10;
+    public float RotateAngle = 10;
 
 	// Use this for initialization
 	void Start () {
@@ -16,5 +20,15 @@ public class MoveTest : MonoBehaviour {
 
         float verticalMove = Input.GetAxis("Vertical");
         transform.Translate(transform.forward * verticalMove * MoveSpeed * deltaTime);
+
+        // Check if the truck is moving.
+        if(verticalMove != 0)
+        {
+            float horizontalMove = Input.GetAxis("Horizontal");
+            transform.Rotate(0, horizontalMove * deltaTime * RotateAngle, 0);
+            transform.FindChild("Truck").Rotate(0, horizontalMove * deltaTime * RotateAngle, 0);
+            transform.FindChild("Main Camera").Rotate(0, horizontalMove * deltaTime * RotateAngle, 0);
+        }
+
 	}
 }
