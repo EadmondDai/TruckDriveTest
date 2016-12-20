0bb44af50ab09cde1637796b2b0aefe0d9ac9150
Yi YIN <kthtes@gmail.com>
2016-12-20 14:19:18 -0800

    --

--- Assets/TruckMotion.cs
+++ Assets/TruckMotion.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// @ By Yi YIN, 2016
+// Controls the movement of the player's truck
+
 public class TruckMotion : MonoBehaviour {
 
 	// Use this for initialization
@@ -13,4 +16,26 @@ public class TruckMotion : MonoBehaviour {
 	void Update () {
 		
 	}
+
+
+    public void OnAccel(float pedal, int shift)
+    {
+       
+    }
+
+    public void OnBrake(float pedal)
+    {
+
+    }
+
+    public void OnReverse(float pedal, int shift)
+    {
+
+    }
+
+    // angle should be -1 ~ 1. -1 means most left, 1 means most right.
+    public void OnTurn(float angle)
+    {
+
+    }
 }
