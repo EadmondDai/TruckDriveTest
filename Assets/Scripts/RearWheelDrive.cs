bf44a5fba672e34d4e75c9b699a1e738778f7e59
Eadmond <eadmond@aliyun.com>
2016-12-24 13:17:11 -0800

    Updated the trailer

--- Assets/Scripts/RearWheelDrive.cs
+++ Assets/Scripts/RearWheelDrive.cs
@@ -14,17 +14,17 @@ public class RearWheelDrive : MonoBehaviour {
 	{
 		wheels = GetComponentsInChildren<WheelCollider>();
 
-		for (int i = 0; i < wheels.Length; ++i) 
-		{
-			var wheel = wheels [i];
+		//for (int i = 0; i < wheels.Length; ++i) 
+		//{
+		//	var wheel = wheels [i];
 
-			// create wheel shapes only when needed
-			if (wheelShape != null)
-			{
-				var ws = GameObject.Instantiate (wheelShape);
-				ws.transform.parent = wheel.transform;
-			}
-		}
+		//	// create wheel shapes only when needed
+		//	if (wheelShape != null)
+		//	{
+		//		var ws = GameObject.Instantiate (wheelShape);
+		//		ws.transform.parent = wheel.transform;
+		//	}
+		//}
 	}
 
 	// this is a really simple approach to updating wheels
@@ -32,31 +32,31 @@ public class RearWheelDrive : MonoBehaviour {
 	// this helps us to figure our which wheels are front ones and which are rear
 	public void Update()
 	{
-		float angle = maxAngle * Input.GetAxis("Horizontal");
-		float torque = maxTorque * Input.GetAxis("Vertical");
+        //float angle = maxAngle * Input.GetAxis("Horizontal");
+        //float torque = maxTorque * Input.GetAxis("Vertical");
 
-		foreach (WheelCollider wheel in wheels)
-		{
-			// a simple car where front wheels steer while rear ones drive
-			if (wheel.transform.localPosition.z > 0)
-				wheel.steerAngle = angle;
+        foreach (WheelCollider wheel in wheels)
+        {
+            //	// a simple car where front wheels steer while rear ones drive
+            //	if (wheel.transform.localPosition.z > 0)
+            //		wheel.steerAngle = angle;
 
-			if (wheel.transform.localPosition.z < 0)
-				wheel.motorTorque = torque;
+            //	if (wheel.transform.localPosition.z < 0)
+            //		wheel.motorTorque = torque;
 
-			// update visual wheels if any
-			if (wheelShape) 
-			{
-				Quaternion q;
-				Vector3 p;
-				wheel.GetWorldPose (out p, out q);
+            // update visual wheels if any
+            if (wheelShape)
+            {
+                Quaternion q;
+                Vector3 p;
+                wheel.GetWorldPose(out p, out q);
 
-				// assume that the only child of the wheelcollider is the wheel shape
-				Transform shapeTransform = wheel.transform.GetChild (0);
-				shapeTransform.position = p;
-				shapeTransform.rotation = q;
-			}
-
-		}
+                // assume that the only child of the wheelcollider is the wheel shape
+                Transform shapeTransform = wheel.transform.GetChild(0);
+                shapeTransform.position = p;
+                shapeTransform.rotation = q;
+            }
+        }
+		
 	}
 }
