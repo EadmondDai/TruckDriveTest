fb7d9f253858a7d5e86d65ebb67f076b12ed1e7f
Eadmond <eadmond@aliyun.com>
2017-01-04 21:49:47 -0800

    Setted up basic script for make turn and other check.

---
+++ Assets/Scripts/ManeuverManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// © By Eadmond, 1.4.2017
+// This script is used to manage the logic part of player maneuver judgement.
+
+// This script access ErrorMassager script.
+// This script access TruckControl script.
+// This script access CameraGaze script.
+
+public class ManeuverManager : MonoBehaviour {
+
+    // This is used to show the massager, all the dynamic massage should be in the script.
+    public ErrorMassage ErrorMassagerScript;
+
+    // This is used to read the state of 
+    public TruckControl TruckControlScript;
+
+    // This is used to check if the player checked sides before make the turn.
+    public CameraGaze CameraGazeScript;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnEnterCornerRight()
+    {
+        // Check if the right turn light is on.
+        if(!TruckControlScript.RightTurnLightOn)
+        {
+
+        }
+
+        // Check if the checked left window
+        // Not sure if this is neccessary.
+        
+
+    }
+
+    public void OnEnterCornerLeft()
+    {
+        if(!TruckControlScript.LeftTurnLightOn)
+        {
+
+        }
+    }
+
+    public void OnLeaveCornerRight()
+    {
+        if (!TruckControlScript.RightTurnLightOn)
+        {
+
+        }
+    }
+
+    public void OnLeaveCornerLeft()
+    {
+        if (!TruckControlScript.LeftTurnLightOn)
+        {
+
+        }
+    }
+
+    public void OnRedLightViolation()
+    {
+
+    }
+
+    public void OnHitCurb()
+    {
+
+    }
+
+    public void OnHitWall()
+    {
+
+    }
+
+}
